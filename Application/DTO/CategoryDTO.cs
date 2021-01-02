f31b8e9e94997e7ce80d8044111c26c95c06d51e
zawette <zawette@gmail.com>
2021-01-02 14:42:10 +0100

    dtos

---
+++ Application/DTO/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTO
+{
+    public class CategoryDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ Application/DTO/ItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.DTO
+{
+    public class ItemDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+        public CategoryDTO Category { get; set; }
+    }
+}
