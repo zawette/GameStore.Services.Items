1d4e4406778903a7499789ac267760fa14580a00
zawette <zawette@gmail.com>
2021-01-26 19:02:58 +0100

    rm comments

--- Infrastructure/Services/MessageBroker.cs
+++ Infrastructure/Services/MessageBroker.cs
@@ -2,7 +2,6 @@ using Application.Events;
 using Application.Services;
 using Infrastructure.Messaging;
 using MassTransit;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,10 +26,8 @@ namespace Infrastructure.Services
         {
             foreach (var @event in events)
             {
-                //Uri uri = new Uri($"exchange:{@event.GetType().Name}?bind=true&queue={_mqOptions.queue}.{@event.GetType().Name}");
-                //var endPoint = await _bus.GetSendEndpoint(uri);
                 var type = @event.GetType();
-                await _bus.Publish(@event,type);
+                await _bus.Publish(@event, type);
             }
         }
     }
